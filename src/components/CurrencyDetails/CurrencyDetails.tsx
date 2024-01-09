bdd0246a16902b735b4a6e28ddb63502e0fd5f29
Weston Vincze <westonvincze@gmail.com>
2024-01-08 22:51:16 -0500

    Adds content and styling for CurrencyDetails view

--- src/components/CurrencyDetails/CurrencyDetails.tsx
+++ src/components/CurrencyDetails/CurrencyDetails.tsx
@@ -1,7 +1,50 @@
+import { useAssetDetails } from "@/hooks/useAssetDetails";
+import styles from "./CurrencyDetails.module.css";
+import { Button } from "../Button";
+import { Line } from "./Line";
+
 interface CurrencyDetailsProps {
   id: string;
 }
 
+/**
+ * Detailed view for Currency based on `id` 
+ */
 export const CurrencyDetails = ({ id }: CurrencyDetailsProps) => {
-  return <>{id}</>
+  const { asset, error } = useAssetDetails(id);
+
+  if (asset === null) return <div />
+
+  return (
+    <article className={styles.details}>
+      <header>
+        <h2>{asset.rank}</h2>
+        <h2>{`${asset.name} (${asset.symbol})`}</h2>
+        <h2>{"<3"}</h2>
+      </header>
+
+      <section>
+        <h2>Price (USD)</h2>
+        <Line label="Price Per Unit:" value={asset.priceUsd} />
+        <Line label="Market Cap:" value={asset.marketCapUsd} />
+      </section>
+
+      <section>
+        <h2>Supply</h2>
+        <Line label="Available to Trade:" value={asset.supply} format="number" />
+        <Line label="Total QTY Issued:" value={asset.maxSupply} format="number" />
+      </section>
+
+      <section>
+        <h2>Last 24 Hours</h2>
+        <Line label="Direction and Value Change:" value={asset.changePercent24Hr} format="percentage" />
+        <Line label="Volume Weighted Average:" value={asset.volumeUsd24Hr} />
+        <Line label="Trading Volume:" value={asset.vwap24Hr} />
+      </section>
+
+      <footer>
+        <Button label="back" href="/" type="link" />
+      </footer>
+    </article>
+  );
 }
---
+++ src/components/CurrencyDetails/Line/Line.tsx
@@ -0,0 +1,34 @@
+import styles from "./Line.module.css";
+import { formatCurrency, formatNumber } from "@/utils/numberFormats";
+
+interface LineProps {
+  label: string,
+  value: string,
+  format?: "currency" | "number" | "percentage",
+}
+
+/**
+ * Represents a single line item of a detailed view 
+ */
+export const Line = ({ label, value, format = "currency" }: LineProps) => {
+  let formattedValue = value;
+
+  switch (format) {
+    case "currency":
+      formattedValue = formatCurrency(Number(value));
+      break;
+    case "number":
+      formattedValue = formatNumber(Number(value));
+      break;
+    case "percentage":
+      formattedValue = `${formatNumber(Number(value))}%`
+      break;
+  }
+
+  return(
+    <div className={styles.line}>
+      <label>{label}</label>
+      <span>{formattedValue}</span>
+    </div>
+  )
+}
