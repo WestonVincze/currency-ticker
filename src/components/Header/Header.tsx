ed40378c08f0f178dd681fbcc933475268cfeeb7
Weston Vincze <westonvincze@gmail.com>
2024-01-09 10:12:40 -0500

    Updates readme, responsive fix, and removes "registration" placeholder (no time)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,8 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <nav className={styles.header}>
-      <h2 className={styles.register}>Welcome, guest</h2>
-      <Link href={"/"}>
-        <h1 className={styles.title}>CryptoWatcher</h1>
+      <Link href={"/"} className={styles.title}>
+        <h1>CryptoWatcher</h1>
       </Link>
     </nav>
   )
